9a8db2749d0859415c73099b9ded76b6617b190f
Carlos Opazo <carlos.opazo@zentagroup.com>
2026-03-12 01:01:14 -0300

    chore: Empty Firebase configuration values.

--- bingo-app/src/app/firebase.config.ts
+++ bingo-app/src/app/firebase.config.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCdIBE_NlUS9yoFzc4m48CPU933qJAV56g",
-    authDomain: "rosita-casinos.firebaseapp.com",
-    projectId: "rosita-casinos",
-    storageBucket: "rosita-casinos.firebasestorage.app",
-    messagingSenderId: "983562277936",
-    appId: "1:983562277936:web:07d13474ba011dc91289f5"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 console.log('🔥 Inicializando Firebase con proyecto:', firebaseConfig.projectId);
